78d709dfbfe3b7df959c7b1fa090dbf043a6be51
Denmark Daya <denmarkamanodaya@gmail.com>
2026-02-06 18:06:00 +0800

    Created API Gateway V2 Module

--- modules/api_gateway/main.tf
+++ modules/api_gateway/main.tf
@@ -1,16 +1,12 @@
-# REST API (root "/" only)
-resource "aws_api_gateway_rest_api" "this" {
-  name = var.api_name
+# HTTP API (v2)
+resource "aws_apigatewayv2_api" "this" {
+  name          = var.api_name
+  protocol_type = "HTTP"
 }
 
-# Deployment (empty API is valid)
-resource "aws_api_gateway_deployment" "this" {
-  rest_api_id = aws_api_gateway_rest_api.this.id
-}
-
-# Stage
-resource "aws_api_gateway_stage" "this" {
-  rest_api_id   = aws_api_gateway_rest_api.this.id
-  deployment_id = aws_api_gateway_deployment.this.id
-  stage_name    = var.stage
+# Default stage
+resource "aws_apigatewayv2_stage" "this" {
+  api_id      = aws_apigatewayv2_api.this.id
+  name        = var.stage
+  auto_deploy = true
 }
--- modules/api_gateway/outputs.tf
+++ modules/api_gateway/outputs.tf
@@ -1,5 +1,7 @@
-output "api_endpoint" {
-  value = "https://${aws_api_gateway_rest_api.this.id}.execute-api.${data.aws_region.current.name}.amazonaws.com/${var.stage}"
+output "api_id" {
+  value = aws_apigatewayv2_api.this.id
 }
 
-data "aws_region" "current" {}
+output "api_endpoint" {
+  value = aws_apigatewayv2_api.this.api_endpoint
+}
